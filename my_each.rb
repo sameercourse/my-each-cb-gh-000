d04cc2b88d01ee0c518b37fea8fb8326c9cc95b0
sameer shaik <sameercourse@gmail.com>
2018-11-20 14:00:46 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,20 @@
-def my_each # put argument(s) here
+def my_each(arr)
   # code here
+  
+  counter=0;
+  while counter<arr.length do 
+    
+    yield(arr[counter])
+    counter+=1 
+    
+  end 
+  return arr
 end
+
+
+yielding_my_each([1,2,3,4]) do |i| 
+  
+  puts i 
+  
+end
+
